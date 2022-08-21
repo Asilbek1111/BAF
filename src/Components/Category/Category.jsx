6bd4395917b5dc6b08fd22af99b2ba92e952f44b
Asilbek1111 <99465065+Asilbek1111@users.noreply.github.com>
2022-08-21 15:34:17 +0300

    contact

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
-import Contact from "../Contact/Contact";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 import "./Category.css";
 import { Link, useParams } from "react-router-dom";
 import Flip from "react-reveal/Flip";
 import { MyContext } from "../../Context/Context";
+import Contact from "../Contact/Contact";
 
 const Category = () => {
   const [products, setProducts] = useState([]);
@@ -18,36 +18,38 @@ const Category = () => {
   }, []);
   var { id } = useParams();
   return (
-    <div className="Category">
-      <Navbar />
-      <h1>
-        {lang === "RU" && "Наши продукты"}
-        {lang === "EN" && "Our products"}
-        {lang === "UZ" && "Bizning mahsulotlar"}
-      </h1>
-      <Flip right cascade>
-        <div className="wrap-cards">
-          {products
-            .filter((e) => e.name_uz && e.image1 && e.category === Number(id))
-            .map((product, i) => (
-              <Link to={`/product=${product.id}`}>
-                <div className="p-card">
-                  <img src={product.image1} alt="" />
-                  <div className="c-bottom">
-                    <p>
-                      {lang === "UZ" && product.name_uz}
-                      {lang === "EN" && product.name_en}
-                      {lang === "RU" && product.name_ru}
-                    </p>
+    <>
+      <div className="Category">
+        <Navbar />
+        <h1>
+          {lang === "RU" && "Наши продукты"}
+          {lang === "EN" && "Our products"}
+          {lang === "UZ" && "Bizning mahsulotlar"}
+        </h1>
+        <Flip right cascade>
+          <div className="wrap-cards">
+            {products
+              .filter((e) => e.name_uz && e.image1 && e.category === Number(id))
+              .map((product, i) => (
+                <Link to={`/product=${product.id}`}>
+                  <div className="p-card">
+                    <img src={product.image1} alt="" />
+                    <div className="c-bottom">
+                      <p>
+                        {lang === "UZ" && product.name_uz}
+                        {lang === "EN" && product.name_en}
+                        {lang === "RU" && product.name_ru}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            ))}
-        </div>
-      </Flip>
+                </Link>
+              ))}
+          </div>
+        </Flip>
+      </div>
       <Contact />
       <Footer />
-    </div>
+    </>
   );
 };
 
